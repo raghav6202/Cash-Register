4a7f0da022f3e56abc8b656a814c4700d9e3aeb4
raghav6202 <raghavchandravs@gmail.com>
2021-08-15 13:07:54 +0530

    initial:commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#billAmt");
+
+const cashGiven = document.querySelector("#cash-given");
+
+const checkButton = document.querySelector("#checkBtn");
+
+console.log(cashGiven.value);
