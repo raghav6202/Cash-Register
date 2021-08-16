861c0d3c4ca96cd0a039889628f757a654c32479
raghav6202 <raghavchandravs@gmail.com>
2021-08-16 11:57:36 +0530

    build

--- app.js
+++ app.js
@@ -4,4 +4,66 @@ const cashGiven = document.querySelector("#cash-given");
 
 const checkButton = document.querySelector("#checkBtn");
 
-console.log(cashGiven.value);
+const message = document.querySelector("#errorMsg");
+
+const noOfNotes = document.querySelectorAll(".noOf-Notes");
+
+const availableDenominations = [2000 ,500 ,100 ,20 ,5 ,1];
+
+
+
+//-----------------------------------
+
+checkButton.addEventListener("click", function checkBillAndCash()
+{
+message.style.display = "none";
+
+//done to clear the msg after changing the value
+
+if(billAmount.value > 0)
+{
+if(cashGiven.value >= billAmount.value  )
+{
+const amountToBeReturned = cashGiven.value - billAmount.value;
+
+calculateChange(amountToBeReturned);
+
+}
+else
+{
+    displayMessage("Cash Given must be greater than bill amount");
+
+}
+}
+
+else
+{
+    displayMessage("Invalid Entry");
+}
+
+});
+
+
+function calculateChange (amountToBeReturned)
+{
+//goes over all the available notes
+    for(let i = 0; i < availableDenominations.length ; i++)
+    {
+
+        //no of notes needed for the denomination
+        const totalNoOfNotes = Math.trunc(amountToBeReturned / availableDenominations[i]);
+
+     amountToBeReturned = amountToBeReturned % availableDenominations[i];
+// displaying
+     noOfNotes[i].innerText = totalNoOfNotes;
+    }
+
+
+}
+
+
+function displayMessage(display)
+{
+    message.style.display = "block"
+    message.innerText = display; 
+}
